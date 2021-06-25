714135b47fe63f400bf551f2109ac9c4fd77a474
ndongbiyoo <86352886+ndongbiyoo@users.noreply.github.com>
2021-06-25 11:51:32 +0100

    Add files via upload

---
+++ .github/workflows/index.py
@@ -0,0 +1,10 @@
+print("Hello World !")
+print('First github project with python code')
+print('NDONG Michel 2021')
+
+def salutation(temp):
+    print('Bonjour '.temp.' Ici Github')
+
+
+nom = "NDONG Michel"
+salutation(nom)
